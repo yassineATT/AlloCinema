ee3c9c6d0a6b44523d0b496a2acd1d748f0f013a
yassineATT <yassineatta@hotmail.com>
2022-09-27 16:14:20 +0200

    render movie

--- app.js
+++ app.js
@@ -12,10 +12,61 @@ const myFetch = (path) => {
 
 myFetch("/search/movie")
     .then(data=> {
+
+        // <div class="movie">
+        //     <div class="movie-description">
+        //         <div class="movie-title">Tomb Raider</div>
+        //         <div class="movie-year">2012</div>
+        //         <div class="movie-realisator">Thomas</div>
+        //         <div class="movie-actor">Tom Cruise</div>
+        //     </div>
+        //     <div class="movie-image">
+        //         <img src="https://m.media-amazon.com/images/I/8129a7-9A7L._AC_SX425_.jpg" alt="movie image" width="200px">
+        //     </div>
+        // </div>
+        const movieListDOM = document.querySelector(".movie-list")
         console.log('res',data.results)
-        const titleDOM = document.querySelector('.movie-title')
-        const title = data.results[0].title
-        titleDOM.innerHTML = title
-        console.log('res',data.results[0].title, titleDOM)
+
+        data.results.forEach(movie => {
+    
+            
+
+            const movieDOM = document.createElement("div")
+            movieDOM.setAttribute("class", "movie")
+
+            const movieDescriptionDOM = document.createElement("div")
+            movieDescriptionDOM.setAttribute("class", "movie-description")
+            movieDOM.appendChild(movieDescriptionDOM)
+
+            const movieTitleDOM = document.createElement("div")
+            movieTitleDOM.setAttribute("class", "movie-title")
+            movieTitleDOM.innerHTML = movie.title
+            movieDescriptionDOM.appendChild(movieTitleDOM)
+
+            const movieRealisatorDOM = document.createElement("div")
+            movieRealisatorDOM.setAttribute("class", "movie-realisator")
+            movieRealisatorDOM.innerHTML = movie.popularity
+            movieDescriptionDOM.appendChild(movieRealisatorDOM)
+
+            const movieDateDOM = document.createElement("div")
+            movieDateDOM.setAttribute("class", "movie-date")
+            movieDateDOM.innerHTML = movie.release_date
+            movieDescriptionDOM.appendChild(movieDateDOM)
+
+
+            const movieImageDOM = document.createElement("img")
+            movieImageDOM.setAttribute("src","https://image.tmdb.org/t/p/w500/uQBbjrLVsUibWxNDGA4Czzo8lwz.jpg")
+            //movieImageDOM.setAttribute("class", "movie-image")
+            //movieImageDOM.innerHTML = movie.popularity
+            movieDOM.appendChild(movieImageDOM)
+
+            movieListDOM.appendChild(movieDOM)
+    
+            // const titleDOM = document.querySelector('.movie-title')
+            // const title = data.results[0].title
+            // titleDOM.innerHTML = title
+            // console.log('res',data.results[0].title, titleDOM)
+        });
+
     })
 
