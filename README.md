483fed5416bc8bfc8ccb70337f2593f47fff1fa5
yassineATT <yassineatta@hotmail.com>
2022-09-27 10:22:38 +0200

    Start mockup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # AlloCinema
+## Test
--- app.js
+++ app.js
@@ -3,16 +3,15 @@ console.log('env', process.env.SECRET_KEY)
 
 const myFetch = (path) => {
     return new Promise( (resolve, reject) => {
-        fetch("https://api.themoviedb.org/3" + path + "?api_key=" + process.env.SECRET_KEY)
+        fetch("https://api.themoviedb.org/3" + path + "?api_key=" + process.env.SECRET_KEY + "&query=Jack+Reacher")
             .then(res => res.json())
             .then(data => resolve(data))
             .catch(e => reject(e))
     }) 
 }
 
-myFetch("/movie/550")
+myFetch("/search/movie")
     .then(data=> {
         console.log('res',data)
     })
-
 
